48742cbbddc4084278c517665a99d202c5c6bd41
Rafael Camacho <rafael@softwarecriollo.com>
2015-09-25 00:37:09 -0430

    adding devise, modifications on user

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ ruby '2.1.2'
 gem 'rails', '4.1.8'
 gem 'rails-api'
 gem 'pg'
+gem 'active_model_serializers'
+gem 'devise'
+gem 'simple_token_authentication' 
 
 group :development, :test do
   gem 'sqlite3'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable
+
   before_save { self.email = email.downcase }
   before_save { self.username = username.downcase }
   validates :full_name, presence: true, length: { maximum: 150 }
