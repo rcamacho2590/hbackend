4c69e14b4c7aa2a3406aee8536836d6ad52e5446
Rafael Camacho <rafael@softwarecriollo.com>
2015-09-26 00:34:38 -0430

    user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,10 +6,59 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user = User.new(username: "username", full_name: "Example User", email: "user@example.com")
+    @user = User.new(username: "username", full_name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password")
   end
 
   test "user should be valid" do
     assert @user.valid?
   end
+
+  test "username should be present" do
+    @user.username = ""
+    assert_not @user.valid?
+  end
+
+  test "full name should be present" do
+    @user.full_name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "username should not be too long" do
+    @user.username = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "fullname should not be too long" do
+    @user.full_name = "a" * 151
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 7
+    assert_not @user.valid?
+  end
+
+
 end
