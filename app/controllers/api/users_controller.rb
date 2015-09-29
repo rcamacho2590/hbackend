6a785d046a3f03993a7aa1330d43e6ae0d45bf49
Rafael Camacho <rafael@softwarecriollo.com>
2015-09-29 14:58:55 -0430

    user controller messages jsons modificated

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,28 +9,46 @@ class Api::UsersController < ApplicationController
   end
 
   def show
-    render json: @user
+    render :status => 200,
+           :json => { :success => true,
+                      :user => UserSerializer.new(@user).serializable_hash
+                    }
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      render :json => user.as_json(:authentication_token=>user.authentication_token, :email=>user.email), :status=>201
+      render :status => 200,
+             :json => { :success => true,
+                        :info => "User Created",
+                        :user => UserSerializer.new(user).serializable_hash
+             }
       return
     else
       warden.custom_failure!
-      render :json => user.errors, :status=>422
+      render :status => 442,
+             :json => { :success => false,
+                        :info => user.errors
+             }
     end
   end
 
   def update
     @user.update_attributes(user_params)
-    render json: @user
+    render :status => 200,
+           :json => { :success => true,
+                      :info => "User Updated",
+                      :user => UserSerializer.new(@user).serializable_hash
+                    }
   end
 
   def destroy
     @user.destroy
-    render json: @user
+    render :status => 200,
+           :json => { :success => true,
+                      :info => "User Deleted",
+                      :user => UserSerializer.new(@user).serializable_hash
+                    }
   end
 
   def find_user
