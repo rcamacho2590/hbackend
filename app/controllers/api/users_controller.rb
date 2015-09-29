af26760f9e7999efe838558d82db8ae911b68068
Rafael Camacho <rafael@softwarecriollo.com>
2015-09-29 01:52:54 -0430

    users controller modifications

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::UsersController < ApplicationController
-  skip_before_filter :authenticate_user!, only: [:index, :show, :create]
+  skip_before_filter :authenticate_user!, only: [:index, :create]
   before_filter :find_user, only: [:show, :update, :destroy]
+  respond_to :json
 
   def index
     @users = User.all
@@ -12,8 +13,14 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    render json: @user
+    user = User.new(user_params)
+    if user.save
+      render :json => user.as_json(:authentication_token=>user.authentication_token, :email=>user.email), :status=>201
+      return
+    else
+      warden.custom_failure!
+      render :json => user.errors, :status=>422
+    end
   end
 
   def update
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   include ActionController::MimeResponds
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
-  respond_to :json
 
-  acts_as_token_authentication_handler_for User
+  acts_as_token_authentication_handler_for User, fallback_to_devise: false
   before_filter :authenticate_user!
 end
