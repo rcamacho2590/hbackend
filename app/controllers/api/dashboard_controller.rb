f7b31b75b6e4ef077d138eb1d139d568c5b5784e
Rafael Camacho <rafael@softwarecriollo.com>
2015-10-15 10:15:18 -0430

    dashboard render json fix

--- app/controllers/api/dashboard_controller.rb
+++ app/controllers/api/dashboard_controller.rb
@@ -3,25 +3,19 @@ respond_to :json
 
   def index
     @posts = paginate Post.all.order("created_at DESC"), per_page: 5
-    render :status => 200,
-           :json => { :success => true,
-             :json => @posts.to_json(
-                            :include => [
-                              :user, :comment => { :only => [:id, :description, :created_at], :include => [:user]}
-                            ])
-           }
+    render json: @posts.to_json(
+                           :include => [
+                             :user, :comment => { :only => [:id, :description, :created_at], :include => [:user]}
+                           ])
   end
 
   def show
     @user = User.find(params[:id])
     @posts = paginate Post.following(@user.following), per_page: 5
-    render :status => 200,
-           :json => { :success => true,
-             :json => @posts.to_json(
-                            :include => [
-                              :user, :comment => { :only => [:id, :description, :created_at], :include => [:user]}
-                            ])
-           }
+    render json: @posts.to_json(
+                           :include => [
+                             :user, :comment => { :only => [:id, :description, :created_at], :include => [:user]}
+                           ])
   end
 
 end
