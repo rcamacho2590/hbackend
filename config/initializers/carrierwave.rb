8de3806e19fc56be40feaa81cb8bd53290d27714
Rafael Camacho <rafael@softwarecriollo.com>
2015-10-16 00:11:23 -0430

    region fog config added

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]                        # required
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                        # required
+    region:                'us-west-1'
   }
   config.fog_directory  = ENV['AWS_BUCKET']                          # required
-  config.fog_public     = false
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+
 end
